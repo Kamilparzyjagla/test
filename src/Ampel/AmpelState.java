13a049fa39496897ee0f9708ab1f0b419568cca8
kamil parzyjagla <201560772+Kamilparzyjagla@users.noreply.github.com>
2025-03-05 10:43:19 +0100

    Klasse AmpelState fertig

---
+++ src/Ampel/AmpelState.java
@@ -0,0 +1,22 @@
+package Ampel;
+
+public class AmpelState {
+    private State aktuellerZustand = State.aus;
+    // Definieren der Zustände
+    public enum State {
+        Grün,
+        Orange,
+        Rot,
+        Rot_Orange,
+        blinkend_Orange,
+        aus
+    }
+
+    public State getAktuellerZustand() {
+        return aktuellerZustand;
+    }
+
+    public void setAktuellerZustand(State aktuellerZustand) {
+        this.aktuellerZustand = aktuellerZustand;
+    }
+}
