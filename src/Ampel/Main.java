58ebbea3f0950bcba81875c79cfb61d912600ba0
kamil parzyjagla <201560772+Kamilparzyjagla@users.noreply.github.com>
2025-03-05 12:20:10 +0100

    Zustand der Ampel Ojektorientierter gemacht

--- src/Ampel/Main.java
+++ src/Ampel/Main.java
@@ -1,13 +1,16 @@
 package Ampel;
 
 import javax.swing.JFrame;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     JFrame jf1;
 
 
-    public Main(Ampel ampel) {
+    public Main(List<Ampel> ampeln) {
 
         jf1 = new JFrame();
         jf1.setSize(400, 600);
@@ -16,7 +19,10 @@ public class Main {
         jf1.setResizable(false);
         jf1.setTitle("Ampel");
 
-        jf1.add(ampel);
+        jf1.setLayout(new FlowLayout());
+        for (Ampel ampel : ampeln) {
+            jf1.add(ampel);
+        }
 
         jf1.setVisible(true);
 
@@ -25,11 +31,20 @@ public class Main {
     public static void main(String[] args) {
 
         AmpelState statusAmpel1 = new AmpelState();
+        AmpelState statusAmpel2 = new AmpelState();
+
         Ampel ampel1 = new Ampel(statusAmpel1);
+        Ampel ampel2 = new Ampel(statusAmpel2);
 
-        new Main(ampel1);
         new Ampelschaltung(statusAmpel1);
+        new Ampelschaltung(statusAmpel2);
 
+        ArrayList<Ampel> ampeln = new ArrayList<Ampel>();
+
+        ampeln.add(ampel1);
+        ampeln.add(ampel2);
+
+        new Main(ampeln);
     }
 
 }
